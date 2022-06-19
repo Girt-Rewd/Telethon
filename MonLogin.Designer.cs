ef55caffccfd94c9aedf10a532d302b0732fe53c
Olivier Raymond <olivier.raymond@gmail.com>
2022-06-19 13:10:47 -0400

    ajout monLogin

---
+++ MonLogin.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Telethon
+{
+    partial class MonLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNom = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.lblMotDePasse = new System.Windows.Forms.Label();
+            this.txtMotDePasse = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(96, 39);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(91, 13);
+            this.lblNom.TabIndex = 0;
+            this.lblNom.Text = "Nom d’utilisateur :";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(204, 36);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(217, 20);
+            this.txtNom.TabIndex = 1;
+            // 
+            // lblMotDePasse
+            // 
+            this.lblMotDePasse.AutoSize = true;
+            this.lblMotDePasse.Location = new System.Drawing.Point(96, 90);
+            this.lblMotDePasse.Name = "lblMotDePasse";
+            this.lblMotDePasse.Size = new System.Drawing.Size(77, 13);
+            this.lblMotDePasse.TabIndex = 0;
+            this.lblMotDePasse.Text = "Mot de passe :";
+            // 
+            // txtMotDePasse
+            // 
+            this.txtMotDePasse.Location = new System.Drawing.Point(204, 87);
+            this.txtMotDePasse.Name = "txtMotDePasse";
+            this.txtMotDePasse.Size = new System.Drawing.Size(217, 20);
+            this.txtMotDePasse.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(204, 142);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(346, 142);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuler.TabIndex = 2;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // MonLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 255);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtMotDePasse);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.lblMotDePasse);
+            this.Controls.Add(this.lblNom);
+            this.Name = "MonLogin";
+            this.Text = "Accueil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label lblMotDePasse;
+        private System.Windows.Forms.TextBox txtMotDePasse;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnAnnuler;
+    }
+}
---
+++ MonLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Telethon
+{
+    public partial class MonLogin : Form
+    {
+        string nomUtilisateur = "telethon2021";
+        string motDePasse = "Don@2021";
+        public MonLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (nomUtilisateur.Equals(txtNom.Text) && motDePasse.Equals(txtMotDePasse.Text))
+            {
+                Interfaceboboche monInterface = new Interfaceboboche();
+                monInterface.Show();
+            }
+            else {
+                Console.WriteLine("pas ok");
+            }
+        }
+    }
+}
