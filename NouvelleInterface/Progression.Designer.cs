42b45d4350a756c055354719b90682bdf8b12c21
Olivier Raymond <olivier.raymond@gmail.com>
2022-06-26 17:14:05 -0400

    interface rétablie

--- NouvelleInterface/Progression.Designer.cs
+++ NouvelleInterface/Progression.Designer.cs
@@ -50,7 +50,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::NouvelleInterface.Properties.Resources.Arbre1;
-            this.pictureBox1.Location = new System.Drawing.Point(117, 275);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(72, 116);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
