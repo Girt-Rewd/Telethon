99a8ae7699eb3a3588b5e4bede70696d6f5e82eb
Olivier Raymond <olivier.raymond@gmail.com>
2022-06-28 19:42:02 -0400

    bugs

--- STELib/GestionnaireSTE.cs
+++ STELib/GestionnaireSTE.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// TODO Appliquer les techniques de gestion d'erreurs et de traitement d'exceptions.
-//
 
 namespace STELib
 {
