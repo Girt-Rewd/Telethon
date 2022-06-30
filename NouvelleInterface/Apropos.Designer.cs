8e9fe2ec18d394684a8464f0b00ae8800dfe08e1
Olivier Raymond <olivier.raymond@gmail.com>
2022-06-30 14:01:50 -0400

    fond apropos

--- NouvelleInterface/Apropos.Designer.cs
+++ NouvelleInterface/Apropos.Designer.cs
@@ -35,6 +35,8 @@
             this.lblFred = new System.Windows.Forms.Label();
             this.lblAuteur01 = new System.Windows.Forms.Label();
             this.lblInfoProjetSTE = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -51,7 +53,7 @@
             // 
             this.lblOlivier.AutoSize = true;
             this.lblOlivier.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblOlivier.Location = new System.Drawing.Point(45, 107);
+            this.lblOlivier.Location = new System.Drawing.Point(19, 106);
             this.lblOlivier.Name = "lblOlivier";
             this.lblOlivier.Size = new System.Drawing.Size(103, 18);
             this.lblOlivier.TabIndex = 2;
@@ -61,7 +63,7 @@
             // 
             this.lblCatherine.AutoSize = true;
             this.lblCatherine.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblCatherine.Location = new System.Drawing.Point(45, 87);
+            this.lblCatherine.Location = new System.Drawing.Point(19, 87);
             this.lblCatherine.Name = "lblCatherine";
             this.lblCatherine.Size = new System.Drawing.Size(100, 18);
             this.lblCatherine.TabIndex = 3;
@@ -71,7 +73,7 @@
             // 
             this.lblFred.AutoSize = true;
             this.lblFred.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblFred.Location = new System.Drawing.Point(45, 68);
+            this.lblFred.Location = new System.Drawing.Point(19, 68);
             this.lblFred.Name = "lblFred";
             this.lblFred.Size = new System.Drawing.Size(100, 18);
             this.lblFred.TabIndex = 4;
@@ -81,7 +83,7 @@
             // 
             this.lblAuteur01.AutoSize = true;
             this.lblAuteur01.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblAuteur01.Location = new System.Drawing.Point(22, 49);
+            this.lblAuteur01.Location = new System.Drawing.Point(19, 49);
             this.lblAuteur01.Name = "lblAuteur01";
             this.lblAuteur01.Size = new System.Drawing.Size(61, 18);
             this.lblAuteur01.TabIndex = 5;
@@ -90,28 +92,39 @@
             // lblInfoProjetSTE
             // 
             this.lblInfoProjetSTE.AutoSize = true;
-            this.lblInfoProjetSTE.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblInfoProjetSTE.Location = new System.Drawing.Point(22, 33);
+            this.lblInfoProjetSTE.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblInfoProjetSTE.Location = new System.Drawing.Point(19, 9);
             this.lblInfoProjetSTE.Name = "lblInfoProjetSTE";
-            this.lblInfoProjetSTE.Size = new System.Drawing.Size(55, 18);
+            this.lblInfoProjetSTE.Size = new System.Drawing.Size(75, 25);
             this.lblInfoProjetSTE.TabIndex = 6;
             this.lblInfoProjetSTE.Text = "STE v1.0";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblOlivier);
+            this.panel1.Controls.Add(this.lblCatherine);
+            this.panel1.Controls.Add(this.lblFred);
+            this.panel1.Controls.Add(this.lblAuteur01);
+            this.panel1.Controls.Add(this.lblInfoProjetSTE);
+            this.panel1.Location = new System.Drawing.Point(55, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 142);
+            this.panel1.TabIndex = 8;
             // 
             // Apropos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(213)))), ((int)(((byte)(147)))));
+            this.BackgroundImage = global::NouvelleInterface.Properties.Resources.Background150pp;
             this.ClientSize = new System.Drawing.Size(884, 506);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.lblOlivier);
-            this.Controls.Add(this.lblCatherine);
-            this.Controls.Add(this.lblFred);
-            this.Controls.Add(this.lblAuteur01);
-            this.Controls.Add(this.lblInfoProjetSTE);
             this.Name = "Apropos";
             this.Text = "Apropos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -123,5 +136,6 @@
         private Label lblFred;
         private Label lblAuteur01;
         private Label lblInfoProjetSTE;
+        private Panel panel1;
     }
 }
